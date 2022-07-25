9d8fcb2734739ac80dc6add9eed56be9c1ac3d13
Tpoveda02 <75458574+Tpoveda02@users.noreply.github.com>
2022-07-25 11:24:02 -0500

    services
    party-candidate

--- registry/src/app/services/candidate/candidate.service.ts
+++ registry/src/app/services/candidate/candidate.service.ts
@@ -1,32 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Candidate } from 'src/app/models/candidate.model';
 @Injectable({
   providedIn: 'root'
 })
 export class CandidateService {
-  URL = "http://127.0.0.1:9998/candidate";
+  URL = "/candidate";
   constructor(private http: HttpClient) { }
 
-  findAll(){
-    return this.http.get<any>(this.URL,{ observe: 'response' });
+  findAll(): Observable<any>{
+    return this.http.get<any>(this.URL);
   }
 
-  findById(id: string){
-    return this.http.get<any>(this.URL + "/" +  id, { observe: 'response' });
+  findById(id: string): Observable<any>{
+    return this.http.get<any>(this.URL + "/" +  id);
   }
 
-  create(id_party:String, candidate:Candidate){
+  create(id_party:String, candidate:Candidate): Observable<any>{
     let url = "/party/" + id_party;
-    return this.http.post<any>(this.URL + url, candidate, { observe: 'response' });
+    return this.http.post<any>(this.URL + url, candidate);
   }
 
-  update(id:String, id_party:String, candidate:Candidate){
+  update(id:String, id_party:String, candidate:Candidate): Observable<any>{
     let url = "/" +  id + "/party/" + id_party;
-    return this.http.put<any>(this.URL + url,candidate,{ observe: 'response' });
+    return this.http.put<any>(this.URL + url,candidate);
   }
 
-  delete(id: string){
-    return this.http.delete<any>(this.URL + "/" +  id,{ observe: 'response' });
+  delete(id: string): Observable<any>{
+    return this.http.delete<any>(this.URL + "/" +  id);
   }
 }
--- registry/src/app/services/party/party.service.ts
+++ registry/src/app/services/party/party.service.ts
@@ -1,31 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Party } from 'src/app/models/party.model';
 @Injectable({
   providedIn: 'root'
 })
 export class PartyService {
 
-  URL = "http://127.0.0.1:9998/party";
+  URL = "/party";
   constructor(private http: HttpClient) { }
 
-  findAll(){
-    return this.http.get<any>(this.URL,{ observe: 'response' });
+  findAll(): Observable<any>{
+    return this.http.get<any>(this.URL);
   }
 
-  findById(id: string){
-    return this.http.get<any>(this.URL + "/" + id, { observe: 'response' });
+  findById(id: string): Observable<any>{
+    return this.http.get<any>(this.URL + "/" + id);
   }
 
-  create(id_party:String, party:Party){
-    return this.http.post<any>(this.URL ,party, { observe: 'response' });
+  create(id_party:String, party:Party): Observable<any>{
+    return this.http.post<any>(this.URL ,party);
   }
 
-  update(id:String, party:Party){
-    return this.http.put<any>(this.URL + "/" +  id,party,{ observe: 'response' });
+  update(id:String, party:Party): Observable<any>{
+    return this.http.put<any>(this.URL + "/" +  id,party);
   }
 
-  delete(id: string){
-    return this.http.delete<any>(this.URL + "/" +  id,{ observe: 'response' });
+  delete(id: string): Observable<any>{
+    return this.http.delete<any>(this.URL + "/" +  id);
   }
 }
