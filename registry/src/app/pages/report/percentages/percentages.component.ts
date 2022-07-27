878ebca76bdd3fe44543c57f6d18ff83aac92f37
Tpoveda02 <75458574+Tpoveda02@users.noreply.github.com>
2022-07-27 09:41:18 -0500

    report - service
    xd

--- registry/src/app/pages/report/percentages/percentages.component.ts
+++ registry/src/app/pages/report/percentages/percentages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from 'src/app/services/report/report.service';
 
 @Component({
   selector: 'app-percentages',
@@ -6,10 +7,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../report.component.css']
 })
 export class PercentagesComponent implements OnInit {
+  data: any;
+  result!:any[];
+  chartOptions: any;
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  // subscription: Subscription;
 
+  // config: AppConfig;
+
+  constructor(private reportService:ReportService) { }
+
+  ngOnInit() {
+    this.getPercentages();
+    this.data = {
+        labels: [this.result[0].name,'B','C'],
+        datasets: [
+            {
+                data: [this.result[0].name, 50, 100],
+                backgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ],
+                hoverBackgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ]
+            }
+        ]
+    };
+
+    // this.config = this.configService.config;
+    // this.updateChartOptions();
+    // this.subscription = this.configService.configUpdate$.subscribe(config => {
+    //     this.config = config;
+    //     this.updateChartOptions();
+    // });
+}
+getPercentages(){
+  this.reportService.findPercentages().subscribe(
+    res=>{
+      this.result = res;
+    }
+  )
+}
+// updateChartOptions() {
+//   this.chartOptions = this.config && this.config.dark ? this.getDarkTheme() : this.getLightTheme();
+// }
+
+getLightTheme() {
+    return {
+        plugins: {
+            legend: {
+                labels: {
+                    color: '#495057'
+                }
+            }
+        }
+    }
+}
+
+getDarkTheme() {
+    return {
+        plugins: {
+            legend: {
+                labels: {
+                    color: '#ebedef'
+                }
+            }
+        }
+    }
+}
 }
--- registry/src/app/services/report/report.service.ts
+++ registry/src/app/services/report/report.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
 
-  constructor() { }
+  URL = "/report/percentages";
+  constructor(private http: HttpClient) { }
+
+  findPercentages(): Observable<any>{
+    return this.http.get<any>(this.URL);
+  }
 }
